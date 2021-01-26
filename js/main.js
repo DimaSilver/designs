75ed85589c0e5d5f28b9742b49cabcac1d69b3b6
DimaSilver <dimka7600@ukr.net>
2021-01-26 06:18:46 +0300

    Застилизовал сэкшены header и first__screen

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 $(function () {
-
-})
+    $('.first__screen-slider').slick( {
+        arrows: false,
+        dots: true,
+        dotsClass: 'first__screen-slider__dots'
+    });
+    $('.menu__btn').on("click", function () {
+       $('.menu__list').toggleClass('menu__list--active');
+    });
+    $('.menu__list').on("click", ".menu__list-item", function() {
+        $(this).addClass('menu__list-item--active').siblings().removeClass('menu__list-item--active');
+    });
+});
