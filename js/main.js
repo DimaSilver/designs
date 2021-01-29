dd437b1c770ed19fb4cf5b97f92cf11640ad0e97
DimaSilver <dimka7600@ukr.net>
2021-01-29 08:53:27 +0300

    Застилизовал сэкшен portfolio(без табов) и services

--- js/main.js
+++ js/main.js
@@ -10,4 +10,17 @@ $(function () {
     $('.menu__list').on("click", ".menu__list-item", function() {
         $(this).addClass('menu__list-item--active').siblings().removeClass('menu__list-item--active');
     });
+    $('.tab').on('click', function (e) {
+        e.preventDefault();
+        $($(this).siblings()).removeClass('tab--active');
+        $($(this).closest('.tabs-wrapper').siblings().find('div')).removeClass('tabs-content--active');
+        $(this).addClass('tab--active');
+        $($(this).attr('href')).addClass('tabs-content--active');
+    });
+    $('.portfolio__slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow : '<button type="button" class="portfolio__slider-arrows portfolio__slider-prev"><img src="images/portfolio__slider-arrow__left.png" alt=""></button>',
+        nextArrow : '<button type="button" class="portfolio__slider-arrows portfolio__slider-next"><img src="images/portfolio__slider-arrow__right.png" alt=""></button>'
+    });
 });
