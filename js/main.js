0d1347d9a91d140083b88d61a08cf34c02042e55
DimaSilver <dimka7600@ukr.net>
2021-02-06 09:48:21 +0300

    Адаптив сэкшенов header и first__screen(768px)

--- js/main.js
+++ js/main.js
@@ -2,10 +2,23 @@ $(function () {
     $('.first__screen-slider').slick( {
         arrows: false,
         dots: true,
-        dotsClass: 'first__screen-slider__dots'
+        dotsClass: 'first__screen-slider__dots',
+        responsive: [
+            {
+                breakpoint: 1001,
+                settings: {
+                    autoplay: true,
+                    infinite: true,
+                    dots: false
+                }
+            }
+        ]
     });
     $('.menu__btn').on("click", function () {
        $('.menu__list').toggleClass('menu__list--active');
+    });
+    $('.menu__close').on("click", function () {
+        $('.menu__list').toggleClass('menu__list--active');
     });
     $('.menu__list').on("click", ".menu__list-item", function() {
         $(this).addClass('menu__list-item--active').siblings().removeClass('menu__list-item--active');
